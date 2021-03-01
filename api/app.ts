cffceadfe0bcda21069d98b52eee39b60af3467d
Felix Chen <felixchen1998@gmail.com>
2021-03-01 11:49:00 -0500

    Update app.ts

--- api/app.ts
+++ api/app.ts
@@ -27,7 +27,7 @@ app.post("/playgrounds", async (req, res) => {
 
   // Start a DIND
   let { stdout, stderr } = await exec(
-    `docker run --privileged -d --network project-calcifer_calcifer --env VIRTUAL_PATH=/playgrounds/${_id} felixchen1998/calcifer-playground:latest`
+    `docker run --privileged -d --network project-calcifer_calcifer --network-alias=playground_${_id} --env VIRTUAL_PATH=/playgrounds/${_id} felixchen1998/calcifer-playground:latest`
   );
 
   // trim newline off..
