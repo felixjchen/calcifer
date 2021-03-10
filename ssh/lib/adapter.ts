d58cd254aaa52bcf910a34c2293b83606bedeead
Felix Chen <felixchen1998@gmail.com>
2021-03-09 23:10:26 -0500

    added FS events

--- ssh/lib/adapter.ts
+++ ssh/lib/adapter.ts
@@ -6,15 +6,17 @@ import mySFTP from "./SFTP";
 export const adapter = async (socket, namespace) => {
   let { host, username, password } = socket.handshake.query;
   let config = { host, username, password };
-  console.log({ host, username, password })
+  console.log({ config });
+  
   let ssh = new SSH2Promise(config);
-  // SFTP
+  let shell = await ssh.shell();
   let sftp = new mySFTP(ssh);
+
+  // Initial List
   let list = await sftp.readdir_r("/root");
   namespace.emit("list", list);
 
-  // Shell
-  let shell = await ssh.shell();
+  // Shell Events
   socket.on("data", async (data) => {
     shell.write(data);
     // If shell modifies FS... the frontend should know!
@@ -23,16 +25,30 @@ export const adapter = async (socket, namespace) => {
       namespace.emit("list", list);
     }
   });
-
-  socket.on('getFile', async (file) => {
-    console.log(file);
-    const content = await sftp.readfile(file.path);
-    socket.emit('sendFile', { node: file, content })
-  })
-
   shell.on("data", (data) => {
     namespace.emit("data", data.toString("binary"));
   });
+
+  // File System Events
+  // https://github.com/mscdex/ssh2-streams/blob/master/SFTPStream.md
+  socket.on("getFile", async (file) => {
+    const content = await sftp.readfile(file.path);
+    socket.emit("sendFile", { node: file, content });
+  });
+  socket.on("writeFile", async (path, content) => {
+    await sftp.writefile(path, content);
+  });
+  socket.on("deleteFile", async (path) => {
+    await sftp.unlink(path);
+  });
+  socket.on("renameFile", async (src, dest) =>{
+    await sftp.rename(src, dest)
+  })
+  socket.on("makeDir", async (path) => {
+    await sftp.mkdir(path);
+  });
+
+  // Close events
   socket.on("disconnect", async () => {
     await ssh.close();
   });
@@ -42,7 +58,4 @@ export const adapter = async (socket, namespace) => {
   shell.on("error", async () => {
     await ssh.close();
   });
-
-  // await sftp.writefile("/root/testing_read_write.txt", "yesssirrr2yesssir");
-  // console.log(await sftp.readfile("/root/testing_read_write.txt"));
 };
