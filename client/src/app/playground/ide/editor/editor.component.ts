a1558406dbc35c7f4dd4303b71ea2e3ecebedd4c
Felix Chen <felixchen1998@gmail.com>
2021-03-12 14:50:44 -0500

    writing to file

--- client/src/app/playground/ide/editor/editor.component.ts
+++ client/src/app/playground/ide/editor/editor.component.ts
@@ -2,33 +2,64 @@ import { Component, OnInit } from '@angular/core';
 import { FileStoreService } from '../../services/file-store.service';
 import { File } from '../../../interfaces/file';
 import { MonacoLanguageService } from '../../services/monaco-language.service';
+import { SocketioService } from '../../../socketio.service';
+import { MonacoEditorModule } from '../../../vendor/ngx-monaco-editor-master/projects/editor/src/public-api';
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
-  styleUrls: ['./editor.component.scss']
+  styleUrls: ['./editor.component.scss'],
 })
 export class EditorComponent implements OnInit {
-  editorOptions = {theme: 'vs-dark', automaticLayout: true, language: 'plaintext'};
+  editorOptions = {
+    theme: 'vs-dark',
+    automaticLayout: true,
+    language: 'plaintext',
+  };
   file: File = {} as any;
-
   editorModel: string = '';
 
   readonly THEME = 'vs-dark';
 
-  constructor(private _fileStore: FileStoreService, private _monacoLanguageService: MonacoLanguageService) {}
+  constructor(
+    private _fileStore: FileStoreService,
+    private _monacoLanguageService: MonacoLanguageService,
+    private _socketService: SocketioService
+  ) {}
 
   ngOnInit(): void {
-    this._fileStore.selectedFile$.subscribe((file: File | null) => {      
+    this._fileStore.selectedFile$.subscribe((file: File | null) => {
       if (file !== null && file.content !== undefined) {
         this._monacoLanguageService.init();
         this.editorOptions = {
           theme: this.THEME,
           automaticLayout: true,
-          language: this._monacoLanguageService.getMonacoLanguageForFile(file.node.filename)
-        }
+          language: this._monacoLanguageService.getMonacoLanguageForFile(
+            file.node.filename
+          ),
+        };
         this.file = file;
         this.editorModel = this.file.content;
+      }
+    });
+  }
+
+  // https://github.com/microsoft/monaco-editor/issues/432
+  // https://github.com/atularen/ngx-monaco-editor/issues/133
+  onInitEditor(editor: any) {
+    if (!editor) {
+      return;
+    }
+    console.log(editor);
+    editor.onDidChangeModelContent((e: any) => {
+      // Only trigger if user types, not if JS loads
+      if (!e.isFlush) {
+        const content = editor.getModel().getValue();
+        this._socketService.socket.emit(
+          'writeFile',
+          this.file.node.path,
+          content
+        );
       }
     });
   }
