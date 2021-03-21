acf982176f5eae1a1b86893778d3435e5f756b58
Felix Chen <felixchen1998@gmail.com>
2021-03-21 17:31:29 -0400

    redirects into edit page

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -1,22 +1,28 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { PlaygroundService } from '../services/playground.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
   loading = false;
   playgroundId: string;
 
-  constructor(private _playgroundService: PlaygroundService) { }
+  constructor(
+    private _playgroundService: PlaygroundService,
+    private _router: Router
+  ) {}
 
   createDashboard(): void {
     this.loading = true;
     this._playgroundService.create().subscribe((response) => {
       this.playgroundId = response._id;
       this.loading = false;
+
+      this._router.navigate(['/edit', `${this.playgroundId}`]);
     });
   }
 }
