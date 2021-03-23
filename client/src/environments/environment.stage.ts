9f6c4e200c4c9215f56ba1d3b605f648add33c64
Felix Chen <felixchen1998@gmail.com>
2021-03-23 01:27:50 -0400

    oh man it works

---
+++ client/src/environments/environment.stage.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  domain: 'http://0.0.0.0',
+  ssh_url: 'http://0.0.0.0',
+  api_url: 'http://0.0.0.0/api',
+  path: '/ssh/socket.io',
+  default_parameters: {
+    host: '.',
+    username: 'root',
+    password: 'playgroundpwWXuHbdwcwEVmyhnJVpYt',
+  },
+};
