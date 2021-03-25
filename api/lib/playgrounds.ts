a851f41697c3061faed7c41a53dc6a45b6df14d4
Felix Chen <felixchen1998@gmail.com>
2021-03-25 10:14:10 -0400

    kind

--- api/lib/playgrounds.ts
+++ api/lib/playgrounds.ts
@@ -18,7 +18,7 @@ const get_playground_command = (id: string, type: string): string => {
 
 const start_kind_playground = async (id: string) => {
   // Kubectl container
-  let command = `docker run --runtime=sysbox-runc -d --network project-calcifer_default --name=${id} --network-alias=${id} -e KUBECONFIG=/.kube/${id}_cluster-config -e LETSENCRYPT_TEST=true -e LETSENCRYPT_HOST=${id}.project-calcifer.ml -e VIRTUAL_HOST=${id}.project-calcifer.ml felixchen1998/calcifer-playground:kind`;
+  let command = `docker run --runtime=sysbox-runc -d --network project-calcifer_default --name=${id} --network-alias=${id} -e KUBECONFIG=/.kube/${id}-cluster-config -e LETSENCRYPT_TEST=true -e LETSENCRYPT_HOST=${id}.project-calcifer.ml -e VIRTUAL_HOST=${id}.project-calcifer.ml felixchen1998/calcifer-playground:kind`;
   await exec(command);
   // K8s cluster
   command = `lib/kindbox create --num-workers=3 --net=project-calcifer_default ${id}-cluster`;
