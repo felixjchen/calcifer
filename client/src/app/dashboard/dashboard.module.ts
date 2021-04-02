a710180f83d3b5fd34f4bae31c6804d7f89a4536
Felix Chen <felixchen1998@gmail.com>
2021-04-01 23:13:11 -0400

    styled sharing component

--- client/src/app/dashboard/dashboard.module.ts
+++ client/src/app/dashboard/dashboard.module.ts
@@ -5,8 +5,7 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import {MatTooltipModule} from '@angular/material/tooltip';
-
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [DashboardComponent],
@@ -15,7 +14,7 @@ import {MatTooltipModule} from '@angular/material/tooltip';
     DashboardRoutingModule,
     MatButtonModule,
     MatProgressSpinnerModule,
-    MatTooltipModule
-  ]
+    MatTooltipModule,
+  ],
 })
-export class DashboardModule { }
+export class DashboardModule {}
