595e0d39c6e41825153136b0cdfbb572838477d6
Felix Chen <felixchen1998@gmail.com>
2021-04-02 13:39:43 -0400

    Update util.ts

--- docsync/lib/util.ts
+++ docsync/lib/util.ts
@@ -2,7 +2,9 @@ import ShareDB from "sharedb";
 
 // Delete entire doc
 const delete_doc_content = (doc: ShareDB.Doc) => {
+  // Fetch fresh doc
   doc.fetch(() => {
+    // Delete entire doc, starting from index 0
     let { content } = doc.data;
     let op = [{ p: ["content", 0], sd: content }];
     try {
@@ -13,8 +15,11 @@ const delete_doc_content = (doc: ShareDB.Doc) => {
   });
 };
 
+// Append to doc
 const add_doc_content = (doc: ShareDB.Doc, content: string) => {
+  // Fetch fresh doc
   doc.fetch(() => {
+    // Append to end
     let end = doc.data.content.length;
     let op = [{ p: ["content", end], si: content }];
     try {
@@ -25,6 +30,7 @@ const add_doc_content = (doc: ShareDB.Doc, content: string) => {
   });
 };
 
+// If the doc falls out of sync with OS, we need to set it
 export const set_doc_content = async (
   doc: ShareDB.Doc,
   content: string
