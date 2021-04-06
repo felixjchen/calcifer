b4f101abfefc0de713a02a23cb36e95e01fa18a9
Felix Chen <felixchen1998@gmail.com>
2021-04-06 01:03:38 -0400

    #32

--- client/src/app/playground/services/monaco-language.service.ts
+++ client/src/app/playground/services/monaco-language.service.ts
@@ -3,18 +3,26 @@ import { Injectable } from '@angular/core';
 declare const monaco: any;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MonacoLanguageService {
-
   languageMap: { [ext: string]: string } = {};
 
   init(): void {
     if (Object.keys(this.languageMap).length !== 0) {
       return;
     }
-    monaco.languages.getLanguages().forEach((language: { id: string, extensions: string[] }) => {
-      language.extensions.forEach((ext: string) => this.languageMap[ext] = language.id);
+    monaco.languages
+      .getLanguages()
+      .forEach((language: { id: string; extensions: string[] }) => {
+        language.extensions.forEach(
+          (ext: string) => (this.languageMap[ext] = language.id)
+        );
+      });
+    //https://stackoverflow.com/questions/56954280/monaco-editor-how-to-disable-errors-typescript
+    monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
+      noSemanticValidation: true,
+      noSyntaxValidation: true,
     });
   }
 
