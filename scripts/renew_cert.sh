c83824efcb8ae9cc94eea2c0b21b6b1ca57c2a11
felixjchen <31393977+felixjchen@users.noreply.github.com>
2022-08-01 14:41:38 -0400

    Update renew_cert.sh

--- scripts/renew_cert.sh
+++ scripts/renew_cert.sh
@@ -1,5 +1,5 @@
 certbot certonly --manual --preferred-challenges=dns --email felixchen1998@gmail.com --server https://acme-v02.api.letsencrypt.org/directory --agree-tos -d *.project-calcifer.ml -d project-calcifer.ml
 rm /root/certs/project-calcifer.ml.key
 rm /root/certs/project-calcifer.ml.crt
-ln -s /etc/letsencrypt/live/project-calcifer.ml/privkey.pem ~/certs/project-calcifer.ml.key 
-ln -s /etc/letsencrypt/live/project-calcifer.ml/fullchain.pem ~/certs/project-calcifer.ml.crt
+cp /etc/letsencrypt/live/project-calcifer.ml/privkey.pem ~/certs/project-calcifer.ml.key 
+cp /etc/letsencrypt/live/project-calcifer.ml/fullchain.pem ~/certs/project-calcifer.ml.crt
